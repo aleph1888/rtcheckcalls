750dd9477217e21a0c95d315adb4d212fed5959d
root <root@centralita.vz.lan>
2013-02-01 16:39:39 +0000

    updated run.sh and README file.

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 cd /home/caedes/rtcheckcalls/
-export PYTHONPATH=/home/caedes/rtcheckcalls/
+export PYTHONPATH=/home/caedes/rtcheckcalls/:/home/caedes/rtcheckcalls/lib/
 python2.7 /usr/bin/twistd -ny main.tac
