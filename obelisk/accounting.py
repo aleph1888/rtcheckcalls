93a65b876a0c89881e1d56877206dccfc2c02ebc
caedes <caedes@centralita.vz.lan>
2013-02-08 05:29:39 +0000

    update user context to freeuser or payuser depending on available credit.

--- obelisk/accounting.py
+++ obelisk/accounting.py
@@ -4,8 +4,8 @@ from decimal import Decimal
 
 from obelisk import charges
 from obelisk.model import Model, User
-from obelisk.resources import sse
 from obelisk.asterisk.model import SipPeer
+from obelisk.asterisk.users import reload_peer
 
 class Accounting(object):
 	def __init__(self):
@@ -15,7 +15,8 @@ class Accounting(object):
 		data = {}
 		users = self.model.query(User)
 		for user in users:
-			data[user.voip_id] = user.credit
+			if user.credit:
+				data[user.voip_id] = user.credit
 		return data
 
 	def get_mana(self, user_ext):
@@ -29,6 +30,22 @@ class Accounting(object):
 		user = self.model.get_user_fromext(user_ext)
 		user.credit = Decimal()
 		self.model.session.commit()
+		self.update_user_context(user)
+
+	def update_user_context(self, user):
+		peer = self.model.query(SipPeer).filter_by(regexten=user.voip_id).first()
+		if not peer:
+			# old user
+			print "user unavailable to update context", user.voip_id
+			return
+		if user.credit > Decimal(0.0) and peer.context == "from-freeuser":
+			peer.context = "from-payuser"
+			self.model.session.commit()
+			reload_peer(peer.name)
+		elif user.credit <= Decimal(0.0) and peer.context == "from-payuser":
+			peer.context = "from-freeuser"
+			self.model.session.commit()
+			reload_peer(peer.name)
 
 	def get_user_for_credit(self, user_ext):
 		user = self.model.get_user_fromext(user_ext)
@@ -48,6 +65,7 @@ class Accounting(object):
 		return user
 	
 	def transfer_credit(self, logged, user_ext, credit):
+		from obelisk.resources import sse
 		if not (credit > 0.0 and logged.credit >= credit):
 			return
 		user = self.get_user_for_credit(user_ext)
@@ -56,17 +74,21 @@ class Accounting(object):
 		logged.credit -= Decimal(credit)
 		user.credit += Decimal(credit)
 		self.model.session.commit()
+		self.update_user_context(user)
+		self.update_user_context(logged)
 		charges.add_charge(user.voip_id, credit, 'transferencia de ' + logged.voip_id)
 		charges.add_charge(logged.voip_id, -credit, 'transferencia a ' + user.voip_id)
 		sse.resource.notify({'credit': float(user.credit), 'user': user.voip_id}, "credit", user)
 		sse.resource.notify({'credit': float(logged.credit), 'user': logged.voip_id}, "credit", logged)
 	
 	def add_credit(self, user_ext, credit):
+		from obelisk.resources import sse
 		user = self.get_user_for_credit(user_ext)
 		if not user:
 			return
 		user.credit += Decimal(credit)
 		self.model.session.commit()
+		self.update_user_context(user)
 		charges.add_charge(user.voip_id, credit)
 		sse.resource.notify({'credit':float(user.credit), 'user':user.voip_id}, "credit", user)
 		return user.voip_id
@@ -78,6 +100,7 @@ class Accounting(object):
 
 		user.credit -= Decimal(credit)
 		self.model.session.commit()
+		self.update_user_context(user)
 
 accounting = Accounting()
 
