3aeaf20101a1710044b56661b623c71643117223
caedes <caedes@centralita.vz.lan>
2013-02-12 20:59:43 +0000

    small fixes.

--- obelisk/prices.py
+++ obelisk/prices.py
@@ -1,4 +1,6 @@
 from incf.countryutils import transformations
+import traceback
+from collections import defaultdict
 
 from obelisk.asterisk.providers import get_providers
 from obelisk.pricechecker import get_winners, load_provider
@@ -10,6 +12,7 @@ def normalize_price(price):
 	return float(price)
 
 def check_prices():
+	output = ""
 	prices = parse_prices()
 	#provider_prices = load_provider(name)
 	providers = get_providers()
@@ -18,10 +21,15 @@ def check_prices():
 	better_deal = ""
 	countries = prices.keys()
 	countries.sort()
+	counters = defaultdict(int)
 	for country in countries:
 		for type in prices[country]:
 			current_rate, provider_code = prices[country][type]
 			if provider_code in providers:
+				try:
+					country_name = transformations.ccn_to_cn(transformations.cca2_to_ccn(country))
+				except:
+					country_name = 'Unknown'
 				if provider_code in loaded_providers:
 					provider_prices = loaded_providers[provider_code]
 				else:
@@ -29,9 +37,11 @@ def check_prices():
 					if names.__class__ ==  str:
 						names = [names]
 					all_prices = []
+					all_providers = set()
 					for name in names:
 						if name in ['freevoipdeal2', 'freevoipdeal3']:
 							name = 'freevoipdeal'
+						all_providers.add(name)
 						# XXX check if prices are the same for given country...
 						provider_prices = load_provider(name)
 						loaded_providers[provider_code] = provider_prices
@@ -39,43 +49,46 @@ def check_prices():
 							country_price = normalize_price(provider_prices[country][type])
 							all_prices.append(country_price)
 						except:
-							print "problem with", name, country, type
+							traceback.print_exc()
+							output += "problem with %s %s %s %s\n" % (name, country, type, name)
 					ok = True
 					if all_prices:
 						last_price = all_prices[0]
 						for aprice in all_prices:
 							if aprice != last_price:
-								print "Price inconsistency", country, type, all_prices
+								output += country_name + " ["+country+"]\t"+type +  " Price inconsistency %s %s\n" % (str(all_prices), str(all_providers))
 								ok = False
 					else:
-						print "No prices for", name, country, type
+						output += "No prices for %s %s %s %s\n" % (name, country, type, str(all_providers))
 				if country in provider_prices and type in provider_prices[country]:
-					try:
-						country_name = transformations.ccn_to_cn(transformations.cca2_to_ccn(country))
-					except:
-						country_name = 'Unknown'
 					current_rate = float(current_rate)
 					names = providers[provider_code]
 					provider_rate = normalize_price(provider_prices[country][type])
 					if current_rate != provider_rate:
 						key = country + " " + type
 						if key in winners:
-							print country_name + " ["+country+"]\t"+type, current_rate, provider_rate, providers[provider_code], winners[key]
+							output += country_name + " ["+country+"]\t"+type + " %s -> %s %s %s\n" % (str(current_rate), str(provider_rate), str(providers[provider_code]), str(winners[key]))
 						else:
-							print country_name + " ["+country+"]\t"+type, current_rate, provider_rate, providers[provider_code]
+							output += country_name + " ["+country+"]\t"+type + " %s -> %s %s\n" % (str(current_rate), str(provider_rate), str(providers[provider_code]))
 					else:
 						key = country + " " + type
 						if key in winners:
 							winner_rate = normalize_price(winners[key][0])
 							if winner_rate < current_rate:
 								better_deal += "%s [%s]\t%s %s %s %s %s\n" % (country_name, country, type, current_rate, providers[provider_code], winner_rate, str(winners[key][1]))
+								for winner in winners[key][1]:
+									counters[winner] += 1
 						
 				else:
-					print country, type, "not available"
+					output += "%s %s not available" % (country, type)
 			else:
-				print "unknown", provider_code
-	print "BETTER DEALS"
-	print better_deal
+				output += "unknown " + str(provider_code)
+	output += "\n\nBETTER DEALS\n"
+	output += better_deal
+	output += "\nWINNER COUNTERS\n"
+	for winner in counters:
+		output += "<b>%s:</b> %s\n" % (winner, counters[winner])
+	return output
 		
 
 if __name__ == "__main__":
