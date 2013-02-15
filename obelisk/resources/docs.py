825e49152d52eb4f3af513cb01d222a7fb465457
caedes <caedes@centralita.vz.lan>
2013-02-15 12:12:38 +0000

    reorganize docs contents into their own folder with an index file.

--- obelisk/resources/docs.py
+++ obelisk/resources/docs.py
@@ -6,10 +6,12 @@ from obelisk.templates import print_template
 class DocsResource(Resource):
     def render_GET(self, request):
 	parts = request.path.split("/")
+	if len(parts) == 2:
+		parts.append('contents')
 	if len(parts) > 2:
 		section = parts[2]
 		try:
-			content = print_template(section, {})
+			content = print_template('docs/' + section, {})
 		except:
 			content = "la seccion no existe"
 		return print_template("content-pbx-lorea", {'content': content})
