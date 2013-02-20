5e02a501c88bd5492c2335d08c030ad97ab19261
caedes <caedes@centralita.vz.lan>
2013-02-20 09:19:50 +0000

    add classes for html tables.

--- obelisk/tools/html.py
+++ obelisk/tools/html.py
@@ -1,9 +1,21 @@
-def format_table(results):
-	output = "<table>\n"
+def format_table(results, attributes={}):
+	if 'class' in attributes:
+		output = "<table class='%s'>\n" % (attributes['class'], )
+	else:
+		output = "<table>\n"
 	for idx, result in enumerate(results):
-		output += "<tr><td>"
-		output += "</td><td>".join(result)
-		output += "</td></tr>\n"
+		if idx % 2:
+			css = 'even'
+		else:
+			css = 'odd'
+		if idx == 0:
+			td = 'th'
+		else:
+			td = 'td'
+		output += "  <tr class='%s'>\n    <%s>" % (css, td)
+		td_split = "</%s>\n    <%s>" % (td, td)
+		output += td_split.join(result)
+		output += "</%s>\n  </tr>\n" % (td, )
 	output += "</table>\n"
 	return output
 
