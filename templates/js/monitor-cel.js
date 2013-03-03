f5f51ab79fde18b73f057d97eeef1f314a0ca4b1
caedes <caedes@centralita.vz.lan>
2013-03-03 09:46:18 +0000

    improve cel monitor.

--- templates/js/monitor-cel.js
+++ templates/js/monitor-cel.js
@@ -8,17 +8,53 @@ function write_div(text, div_id) {
 		text: text}).appendTo(div_id).hide().fadeIn().delay(45000).slideUp()
 }
 
+function clear_events() {
+	$('#cel_events').children().fadeOut(300, function() {$(this).remove()})
+}
+
+function do_ping() {
+	console.log("ping")
+	$.get("/")
+	setTimeout('do_ping()',60000);
+}
+
 events_head = false
+current_linkedid = null
+total_events = 0
 function fill_head(data) {
 	var text = "<tr>"
 	for (variable in data) {
 		text += "<td>"+variable+"</td>";
 	}
 	text += "</td>"
-		$('#cel_events_head').html(text)
+	var node = $(text);
+	node.stamp = data.linkedid
+	node.data('stamp', data.linkedid)
+	$('#cel_events_head').html(node.fadeIn())
 	events_head = true
 	
 }
+
+
+function show_all() {
+	$('#current_linkedid').html('all')
+	$('#cel_events').children().fadeIn()
+	current_linkedid = null
+}
+
+function show_linkedid(linkedid) {
+	current_linkedid = linkedid;
+	$('#current_linkedid').html(current_linkedid)
+	$.each($('#cel_events').children(), function(index, elmt) {
+		if ($(elmt).data('linkedid') == current_linkedid) {
+			$(elmt).fadeIn()
+		}
+		else {
+			$(elmt).fadeOut()
+		}
+	})
+}
+
 function on_event(data) {
 	delete data.amaflags
 	if (!events_head) {
@@ -26,10 +62,23 @@ function on_event(data) {
 	}
 	var text = "<tr>"
 	for (variable in data) {
-		text += "<td>"+data[variable]+"</td>";
+		if (variable == 'linkedid') {
+			text += "<td><a href='javascript:show_linkedid(\""+data[variable]+"\")'>"+data[variable]+"</a></td>";
+		}
+		else {
+			text += "<td>"+data[variable]+"</td>";
+		}
 	}
 	text += "</td>"
-		$('#cel_events').prepend(text)
+	var node = $(text);
+	node.data('linkedid', data.linkedid)
+	if ((current_linkedid == null) || (current_linkedid == data.linkedid)) {
+		$('#cel_events').prepend(node.fadeIn())
+	} else {
+		node.css('display', 'none').prependTo('#cel_events')
+	}
+	total_events += 1
+	$('#total_events').html(total_events)
 	
 }
 
@@ -41,6 +90,7 @@ $(document).ready( function () {
 	root_circle = paper.circle(320, 240, 60).animate({fill: "#223fa3", stroke: "#000", "stroke-width": 80, "stroke-opacity": 0.5}, 2000, "backOut");*/
 
 	if (!!window.EventSource) {
+	  setTimeout('do_ping()',60000);
 	  var source = new EventSource('/sse');
 	} else {
 	  // Result to xhr polling :(
@@ -50,12 +100,11 @@ $(document).ready( function () {
 	}, false);
 
 	source.addEventListener('callmonitor', function(e) {
-		var data = JSON.parse(e.data);
-		//write_div(e.data, '#default')
+	//	var data = JSON.parse(e.data);
 	}, false);
 	source.addEventListener('credit', function(e) {
-		console.log(e.data);
 		var data = JSON.parse(e.data);
+		write_div(e.data, '#default')
 	}, false);
 
 	source.addEventListener('peers', function(e) {
@@ -64,12 +113,10 @@ $(document).ready( function () {
 
 	source.addEventListener('peer', function(e) {
 		var peer_event = JSON.parse(e.data);
-		console.log(peer_event)
 	}, false);
 
 
 	source.addEventListener('rtcheckcalls', function(e) {
-		console.log(e.data);
 		var data = JSON.parse(e.data);
 		on_event(data)
 	}, false);
